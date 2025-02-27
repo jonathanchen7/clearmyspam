a4be3434e6f3569c8f28297279e07bfc8d0fa161
Jonathan Chen <jonathanschen28@gmail.com>
2025-02-26 16:35:22 -0800

    update rate limit retry behavior

--- app/jobs/dispose_emails_job.rb
+++ app/jobs/dispose_emails_job.rb
@@ -5,17 +5,16 @@ class DisposeEmailsJob < ApplicationJob
 
   good_job_control_concurrency_with(
     key: -> { "#{arguments.find { |arg| arg.is_a?(User) }.id}:dispose" },
-    perform_limit: 1,
-    perform_throttle: [2, 2.seconds]
+    perform_limit: 1
   )
 
   retry_on Google::Apis::RateLimitError, Google::Apis::ServerError,
-           wait: ->(executions) { (executions * 2) + 15 + (rand(10) * (executions + 1)) },
-           attempts: 10
+           wait: ->(_) { rand(5..60) },
+           attempts: 15
 
   retry_on Google::Apis::ClientError,
-           wait: ->(executions) { (executions * 2) + 15 + (rand(10) * (executions + 1)) },
-           attempts: 3
+           wait: ->(_) { rand(5..60) },
+           attempts: 5
 
   # If you change the signature of this method, make sure to also update the good_job concurrency controls.
   def perform(user, email_threads, archive:)
