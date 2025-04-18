69bfa0e01a7821070d09890c4fe01a93a96f979d
Jonathan Chen <jonathanschen28@gmail.com>
2025-04-17 23:05:09 -0700

    modelize Gmail client and add get_unique_senders!

--- app/models/gmail/client.rb
+++ app/models/gmail/client.rb
@@ -9,6 +9,8 @@ module Gmail
     THREAD_DETAILS_BATCH_SIZE = 20
     DISPOSE_BATCH_SIZE = 15
 
+    attr_reader :user
+
     class << self
       def get_inbox_metrics!(user)
         set_client_authorization(user)
@@ -18,21 +20,8 @@ module Gmail
         [response.threads_total, response.threads_unread]
       end
 
-      # Returns the number of threads that match the provided query.
-      #
-      # @param user [User] The user whose threads are being counted.
-      # @param query [String, nil] An optional query string to filter the threads.
-      # @return [Integer] The count of threads matching the query (max 500).
-      def get_thread_count!(user,
-                            query:,
-                            label_ids: ["INBOX"],
-                            unread_only: false)
-        set_client_authorization(user)
-
-        label_ids << "UNREAD" if unread_only
-
-        response = client.list_user_threads("me", max_results: 500, label_ids: label_ids, q: query)
-        response.threads&.size || 0
+      def get_thread_count!(user, query:, label_ids: ["INBOX"], unread_only: false)
+        new(user).get_thread_count!(query:, label_ids:, unread_only:)
       end
 
       def get_threads!(user,
@@ -141,6 +130,85 @@ module Gmail
 
         google_threads
       end
+    end
+
+    def initialize(user)
+      @user = user
+    end
+
+    def get_unique_senders!(max_results: Rails.configuration.sync_fetch_count, page_token: nil)
+      set_client_authorization
+
+      response = client.list_user_threads("me", label_ids: ["INBOX"], max_results:, page_token:)
+
+      return [] unless response.threads.any?
+
+      gmail_thread_ids = response.threads.map(&:id)
+      google_threads = gmail_thread_ids.each_slice(THREAD_DETAILS_BATCH_SIZE).flat_map.with_index do |batch, index|
+        sleep(1) unless index.zero?
+        get_threads_batch_request(batch)
+      end
+
+      senders = google_threads.each_with_object({}) do |t, hash|
+        sender = Sender.extract_from_gmail_thread(t)
+
+        hash[sender.id] = sender if sender.present? && (!hash.key?(sender.id) || sender.newer_than?(hash[sender.id]))
+      end
+
+      # TODO: Mark protected senders.
+
+      [senders.values, response.next_page_token]
+    end
+
+      # Returns the number of Gmail threads that match the provided query.
+      #
+      # @param query [String, nil] An optional query string to filter the threads.
+      # @return [Integer] The count of threads matching the query (max 2500).
+      def get_thread_count!(query:, label_ids: ["INBOX"], unread_only: false)
+        set_client_authorization
+
+        label_ids << "UNREAD" if unread_only
+        response = client.list_user_threads("me", max_results: 500, label_ids: label_ids, q: query)
+        return 0 unless response.threads&.any?
+
+        total_count = response.threads.size
+        while response.next_page_token && total_count < 2500
+          response = client.list_user_threads("me", max_results: 500, label_ids: label_ids, q: query, page_token: response.next_page_token)
+          break unless response.threads&.any?
+          total_count += response.threads.size
+        end
+
+        total_count
+      end
+
+    private
+
+    def set_client_authorization
+      user.refresh_google_auth!
+
+      client.authorization = user.google_access_token
+    end
+
+    def get_threads_batch_request(gmail_thread_ids)
+      Rails.logger.info("Fetching batch of #{gmail_thread_ids.size} threads")
+      gmail_threads = []
+      client.batch do |gmail|
+        gmail_thread_ids.each do |tid|
+          # threads.get uses 10 quota units. Batch requests should be ~20 emails to avoid rate limits.
+          gmail.get_user_thread("me", tid,
+                                format: "metadata",
+                                metadata_headers: %w[From Date Subject]) do |result, error|
+            if error
+              Rails.logger.error("Error fetching thread #{tid}: #{error}".on_red)
+              raise error
+            else
+              gmail_threads << result
+            end
+          end
+        end
+      end
+
+      gmail_threads
     end
   end
 end
--- app/models/sender.rb
+++ app/models/sender.rb
@@ -9,6 +9,20 @@ class Sender
 
   attr_reader :email, :name, :as_of_date
 
+  class << self
+    def extract_from_gmail_thread(gmail_thread)
+      latest_message = gmail_thread.messages.first
+      headers = latest_message.payload.headers
+      raw_sender = EmailThread.fetch_gmail_header(headers, "From")
+      raw_date = EmailThread.fetch_gmail_header(headers, "Date")
+
+      new(raw_sender, as_of_date: raw_date)
+    rescue => e
+      Rails.logger.error("Error extracting sender from Gmail thread #{gmail_thread.id}: #{e}".on_red)
+      nil
+    end
+  end
+
   def initialize(raw_sender, as_of_date:)
     @email = raw_sender[FORMATTED_EMAIL_REGEX, 1] || raw_sender[EMAIL_REGEX]
     raise ArgumentError, "Email could not be extracted from raw_sender #{raw_sender}" unless email
