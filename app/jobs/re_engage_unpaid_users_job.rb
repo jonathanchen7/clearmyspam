229fdf42e638f925bd97b1651d6598a6445514eb
Jonathan Chen <jonathan@levanta.io>
2025-11-14 00:49:50 -0800

    add re-engagement reminder job

---
+++ app/jobs/re_engage_unpaid_users_job.rb
@@ -0,0 +1,31 @@
+require "stripe"
+
+class ReEngageUnpaidUsersJob < ApplicationJob
+  queue_as :default
+
+  attr_reader :date_range
+
+  def perform(date_range: 1.week.ago..Time.current, sleep_interval: 1.second)
+    @date_range = date_range
+
+    active_unpaid_users.each do |user|
+      puts "USER: #{user.email}"
+      next if user.sent_email?("re_engagement") && user.sent_email?("re_engagement_reminder")
+
+      puts "SENDING AN EMAIL!"
+      if !user.sent_email?("re_engagement")
+        SentEmail.send_re_engagement_email!(user)
+      elsif user.sent_emails.find_by(email_type: "re_engagement").sent_at < 3.days.ago
+        SentEmail.send_re_engagement_reminder_email!(user)
+      end
+
+      sleep sleep_interval
+    end
+  end
+
+  private
+
+  def active_unpaid_users
+    @active_unpaid_users ||= User.unpaid.where(last_login_at: date_range, send_marketing_emails: true)
+  end
+end
